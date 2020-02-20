e5f95f5d18865aabe502971764cac1cc5d783edb
Stanislav Opletal <stanislav.opletal@gmail.com>
2020-02-20 10:09:04 +0100

    fix update, vanilla stop music

---
+++ client.lua
@@ -0,0 +1,5 @@
+Citizen.CreateThread(function()
+    SetStaticEmitterEnabled('LOS_SANTOS_VANILLA_UNICORN_01_STAGE', false)
+    SetStaticEmitterEnabled('LOS_SANTOS_VANILLA_UNICORN_02_MAIN_ROOM', false)
+    SetStaticEmitterEnabled('LOS_SANTOS_VANILLA_UNICORN_03_BACK_ROOM', false)
+end)
